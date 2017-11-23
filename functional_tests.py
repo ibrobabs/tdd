fb8962bab6fb27c6c18bd3ab8c5d98e363a5d1cf
ibrobabs <babskolawole@gmail.com>
2017-11-23 01:23:35 -0700

    update

---
+++ functional_tests.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import unittest
+
+
+
+
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retreive_it_later(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.asserTin('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ =- '__main__':
+    unittest.main(warnings='ignore')
