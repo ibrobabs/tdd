f6410fe3610e8200c653f7efa35bbf30a7a94cfc
ibrobabs <babskolawole@gmail.com>
2017-11-23 05:04:56 -0700

    Model for Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -8,7 +8,6 @@ import time
 
 
 
-
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -19,6 +18,13 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+
     def test_can_start_a_list_and_retreive_it_later(self):
 
         self.browser.get('http://localhost:8000')
@@ -27,25 +33,28 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
 
         inputbox.send_keys('Buy peacock feathers')
 
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peaccok feathers')
+
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
 
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock faethers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peaccok feathers')
+        self.check_for_row_in_list_table('1: Buy peaccok feathers to make a fly')
+
 
         self.fail('Finish the test')
 
