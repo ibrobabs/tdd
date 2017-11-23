8d470b7bc1c752f71f3ca57673ace6a564aba5fe
ibrobabs <babskolawole@gmail.com>
2017-11-23 04:27:18 -0700

    update functional test

--- lists/views.py
+++ lists/views.py
@@ -6,4 +6,7 @@ from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        new_text = request.POST.get('item_text', '')
+        return render(request, 'home.html', {'new_item_text':new_text})
     return render(request, 'home.html')
