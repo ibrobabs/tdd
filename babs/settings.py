59e62c3f5ef22613cc23ae37311ea31d72c5c729
ibrobabs <babskolawole@gmail.com>
2017-11-23 20:51:31 -0700

    set STATIC_ROOT in settings and disable admin

--- babs/settings.py
+++ babs/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -107,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
